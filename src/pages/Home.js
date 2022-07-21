5883f9abe8756d2dc96402bd4a2bdd501da7481e
erika mayoute <e.mayoute44@gmail.com>
2022-07-21 13:54:27 +0200

    App Pokemon en react

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import PokemonList  from '../components/PokemonList';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <PokemonList/>
+            <h1>Bienvenu sur la page d'accueil</h1>
+        </div>
+    );
+};
+export default Home;
